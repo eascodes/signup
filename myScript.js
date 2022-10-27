1de04056f6c9902b3f45f54b8d6e136335ae6bd6
Emily <easchoettler@gmail.com>
2022-10-27 14:49:14 -0400

    Add JS function to confirm that passwords match and adjust style accordingly

--- myScript.js
+++ myScript.js
@@ -1,6 +1,26 @@
-const pwd = document.querySelector('#pwd-input');
+//Create error message
 const errorMsg = document.createElement('p');
 errorMsg.textContent = "* Passwords do not match";
-pwd.appendChild(errorMsg);
 errorMsg.classList.add("error-msg");
+
+//Set variables for password inputs
+const pwd = document.querySelector('#pwd-input');
+pwd.appendChild(errorMsg);
+const pwdBox1 = document.querySelector('#pwd');
+const pwdBox2 = document.querySelector('#confirm-pwd');
+let pwdinput1 = "";
+let pwdinput2 = "";
+
+function checkPwd() {
+    pwdinput1 = document.querySelector('#pwd').value;
+    pwdinput2 = document.querySelector('#confirm-pwd').value;
+    
+    if (pwdinput2 != "") {
+        if (pwdinput1 === pwdinput2) {
+            pwd.removeChild(errorMsg);
+            pwdBox1.classList.remove("error");
+            pwdBox2.classList.remove("error");
+        }
+    }
+}
 
